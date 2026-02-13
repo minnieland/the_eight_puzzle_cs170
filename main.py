dbc9fd13190bcaca2e44dd5204ba7ec085bcc292
Jasmine Trình <168297703+minnieland@users.noreply.github.com>
2026-02-12 23:16:04 -0800

    added comments

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ hard =      [[1,2,3],
             [5,0,6],
             [4,7,8]]
 
+#goal state for 8-puzzle
 eight_goal_state = [[1,2,3],
                     [4,5,6],
                     [7,8,0]]
@@ -26,32 +27,34 @@ eight_goal_state = [[1,2,3],
 #represents the state of the puzzle, lower cost in priority queue
 class Node:
     def __init__(self,state,parent=None, action=None, cost=0, depth=0, heuristic=0):
-        self.state=state
-        self.parent=parent
-        self.action=action
-        self.cost=cost #g(n)
-        self.heuristic=heuristic #h(n)
+        self.state=state #current puzzle config
+        self.parent=parent #keep parent for path tracing
+        self.action=action #moves to reach curr state
+        self.cost=cost #g(n) cost from start to curr node
+        self.heuristic=heuristic #h(n), calculated or hardcoded to 0
         self.depth=depth
-        
+    
+    #f(n)=g(n)+h(n) is the total cost when using each algo    
     def f(self):
         return self.cost + self.heuristic
-        
+    #lower f(n) cost means better path to goal using A*    
     def __lt__(self,other):
         return self.f() < other.f()
         
 class Eight_Puzzle:
         
-#goal test   
     def __init__(self,initial_state):
         self.initial_state=initial_state
         self.goal_state=eight_goal_state 
         
+#goal test, check if curr is equal to goal state
     def is_goal(self, state):
         return state == self.goal_state
     
+    #generate all successors from curr
     def successors(self,state):
         successors=[]
-    
+        #find blank tile, where tile value = 0
         for r in range(3):
             for c in range(3):
                 if state[r][c] == 0:
@@ -65,24 +68,31 @@ class Eight_Puzzle:
             'L':(0,-1)
         }
     
+        #try all moves possible
         for action, (dr,dc) in moves.items():
             new_row=zero_row+dr
             new_col=zero_col+dc
+            
+            #make sure new position is in bounds
             if 0<= new_row < 3 and 0 <= new_col < 3:
-                new_state=[row.copy() for row in state]
-                # new_state=state.copy()
+                new_state=[row.copy() for row in state] #copy the state
                 
+                #swap blank with adjacent tile
                 new_state[zero_row][zero_col],new_state[new_row][new_col]=\
                     new_state[new_row][new_col],new_state[zero_row][zero_col]
-                #step cost = 1
-                successors.append((action,new_state,1))     
+                #step cost = 1, add to sucessory list 
+                successors.append((action,new_state,1))   
+                  
         return successors   
 
 #create child nodes (all possible next moves from curr node)
 def expand(node, problem, heuristic_fn=None):
     children=[]
+    #get all possible next states
     for action, next_state, step_cost in problem.successors(node.state):
+       #calculate heursitic, if there isnt any, set it to zero / uniform cost
         h = heuristic_fn(next_state) if heuristic_fn else 0
+        #make a childnode
         child = Node(
             state=next_state,
             parent=node,
@@ -95,7 +105,7 @@ def expand(node, problem, heuristic_fn=None):
         children.append(child)
     return children
 
-#how far away is each tile
+#how far away is each tile from its goal pos (sum)
 def manhattan_distance(state):
     distance=0
     
@@ -106,33 +116,35 @@ def manhattan_distance(state):
             tile=eight_goal_state[row][column]
             goal_positions[tile]=(row,column)
             
-    #calc manhattan
+    #calc manhattan for each tile
     for row in range(3):
         for column in range(3):
             tile=state[row][column]
-            if tile !=0:
+            if tile !=0: #count all tiles but the blank
                 goal_row,goal_column=goal_positions[tile]
-                #equation
+                #equation =|x1 - x2| + |y1 - y2|
                 distance+=abs(row-goal_row)+abs(column-goal_column)
     return distance
 
-#counts how many tiles are wrong
+#counts how many tiles are wrong positions
 def misplaced_tile(state):
     goal= eight_goal_state
     misplaced=0
     
+    #in the 3x3 check the positions of each 
     for row in range(3):
         for column in range(3):
+            #count the tile only if its not the blank and in an incorrect position from goal
             if state[row][column] != 0 and state[row][column] != goal[row][column]:
                 misplaced +=1
-        return misplaced
+    return misplaced
 
 #a*
 def a_star(problem, heuristic_fn=None):
-    open_queue= []
-    
+    open_queue= [] #priorty queue
+    #calculate heursitic, if there isnt any, set it to zero / uniform cost
     h= heuristic_fn(problem.initial_state) if heuristic_fn else 0
-    
+    #make a start node
     start_node = Node(
         state=problem.initial_state,
         parent=None,
@@ -141,15 +153,15 @@ def a_star(problem, heuristic_fn=None):
         heuristic=h,
         depth=0
     )
-
+    #start node goes into prio queue
     heapq.heappush(open_queue,start_node)
-    visited= set()
+    visited= set()  #set of explored
     node_expanded=0
     max_queue_size=0
     
     while open_queue:
         max_queue_size=max(max_queue_size, len(open_queue))
-        current = heapq.heappop(open_queue)
+        current = heapq.heappop(open_queue) #get node with lowkey f(n) cost
         
         #goal test, see if you reached it
         if problem.is_goal(current.state):
@@ -157,18 +169,18 @@ def a_star(problem, heuristic_fn=None):
             print("Max queue size:",max_queue_size)
             print("Depth of solution:",current.depth) #not needed but easier for testing
             return current
-        
+        #curr state is marked visited
         current_tuple = tuple(tuple(row) for row in current.state)
         visited.add(current_tuple)
         
-        #use the expand function
+        #use the expand function on curr node, children are added to queue
         for child in expand(current,problem,heuristic_fn):
             child_tuple=tuple(tuple(row) for row in child.state)
             if child_tuple not in visited:
                 heapq.heappush(open_queue,child)        
         node_expanded+=1
         
-    return None
+    return "No solution is found."
     
     
 #puzzles to test already embedded in the system
@@ -226,7 +238,7 @@ def main():
         problem=Eight_Puzzle(puzzle_list)
         choose_algorithm(problem)
         
-        # select...
+        # select and enter own custom puzzle
     if puzzle_mode == '2':
         print("Enter your custom puzzle, using zero to represent the blank." +
               "Please only enter whole numbers from 0-8. Add a space in between the numbers." +
@@ -236,16 +248,18 @@ def main():
         puzzle_second_row=input("Enter the second row: ")
         puzzle_third_row=input("Enter the third row: ")
         
+        #split input strings into lists
         puzzle_first_row=puzzle_first_row.split()
         puzzle_second_row=puzzle_second_row.split()
         puzzle_third_row=puzzle_third_row.split()
         
+        #convert strings to int
         for i in range(0,3):
             puzzle_first_row[i]=int(puzzle_first_row[i])
             puzzle_second_row[i]=int(puzzle_second_row[i])
             puzzle_third_row[i]=int(puzzle_third_row[i])
         
-        #tuple
+        #turn into 2d list
         user_puzzle=[puzzle_first_row,puzzle_second_row,puzzle_third_row]
         print_puzzle(user_puzzle)
         problem=Eight_Puzzle(user_puzzle)
